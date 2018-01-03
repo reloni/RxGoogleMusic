8ae65e7ae66bb5efd0b87870a012ece8cde9e1ee
Anton Efimenko <reloni@ya.ru>
2018-01-03 18:29:51 +0300

    Add convenience init to GMusicClient

--- RxGoogleMusic/GMusicClient.swift
+++ RxGoogleMusic/GMusicClient.swift
@@ -15,10 +15,14 @@ final public class GMusicClient {
 	public let locale: Locale
 	public let tier: String
 	
-	public init(session: URLSession = URLSession.shared,
-				locale: Locale = Locale.current,
-				tier: String = "aa",
-				baseUrl: URL = URL(string: "https://mclients.googleapis.com/sj/v2.5")!) {
+	convenience init(session: URLSession = URLSession.shared, locale: Locale = Locale.current) {
+		self.init(session: session, locale: locale, tier: "aa", baseUrl: URL(string: "https://mclients.googleapis.com/sj/v2.5")!)
+	}
+	
+	init(session: URLSession,
+				locale: Locale,
+				tier: String,
+				baseUrl: URL) {
 		self.session = session
 		self.locale = locale
 		self.tier = tier
