24af8c5f32fff40498522d7e085b3c88390f4e93
Anton Efimenko <reloni@ya.ru>
2018-01-04 18:10:54 +0300

    Use auth advide for obtaining login url

--- ExampleApp/ExampleApp/ViewController.swift
+++ ExampleApp/ExampleApp/ViewController.swift
@@ -38,22 +38,26 @@ lib_ver=1.0&
 wv_mode=1
 */
 class ViewController: UIViewController {
-	let url: URL = {
+	let working_gmusic: URL = {
 		let host = "accounts.google.com"
 		let path = "embedded/setup/v2/safarivc"
 		let scope = "scope=https://www.google.com/accounts/OAuthLogin+https://www.googleapis.com/auth/userinfo.email"
 		let clientId = "client_id=936475272427.apps.googleusercontent.com"
 		let delegated_client_id = "delegated_client_id=228293309116.apps.googleusercontent.com"
-		let hl = "hl=\(Locale.current.identifier)"
+		let hl = "hl=ru"
 		let device_name = "device_name=iPhone"
-		let auth_extension = "auth_extension=ADa53XKad2bqcW2HiRZ08aB8YECipSG9yVGUP8AEoWGR359nJ6yOTPXbNil2D5CiT67m48gBMLUp7ing2c03fHCZ_nk9uHFqjwv5IWlQdzMBsfyBqUg5sF4"
+		//let auth_extension = "auth_extension=ADa53XKad2bqcW2HiRZ08aB8YECipSG9yVGUP8AEoWGR359nJ6yOTPXbNil2D5CiT67m48gBMLUp7ing2c03fHCZ_nk9uHFqjwv5IWlQdzMBsfyBqUg5sF4"
+		let auth_extension = "auth_extension=ADa53XLeimIbXEKskF0--dVQs-RK0qBMztk7u_H88f7js1d18e_8X_L_ciLugPytFEgfn9UPwrBM6lJ0wyqt3bwaTw7urH4KB_GEDNjsruR9AS_JOGfqHZo"
 		let system_version = "system_version=\(UIDevice.current.systemVersion)"
 		let appVersion = "app_version=1.0"
 		let kdlc = "kdlc=1"
 		let kdac = "kdac=1"
-		let `as` = "as=237d84991ca1076"
+//		let `as` = "as=237d84991ca1076"
+		let `as` = "as=-6a1802a0a7cd8c2b"
 		let redirectUri = "redirect_uri=com.google.sso.228293309116:/authCallback?login%3Dcode"
-		let params = [scope, clientId, delegated_client_id, hl, device_name, auth_extension, system_version, appVersion, `as`, redirectUri].joined(separator: "&")
+		let sarp = "sarp=1"
+		let wvmode = "wv_mode=0"
+		let params = [scope, clientId, delegated_client_id, hl, device_name, auth_extension, system_version, appVersion, `as`, sarp, redirectUri, wvmode].joined(separator: "&")
 		return URL(string: "https://\(host)/\(path)?\(params)")!
 	}()
 	
@@ -62,7 +66,8 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		print(String("228293309116-bs4u7ofpm4p6p6da7i1jkan3hfr6h38o".reversed()))
+		print("working: \(working_gmusic)")
+		print("not working: https://accounts.google.com/embedded/setup/v2/safarivc?as=347a15ec856b0c75&scope=https://www.google.com/accounts/OAuthLogin+https://www.googleapis.com/auth/userinfo.email&client_id=936475272427.apps.googleusercontent.com&redirect_uri=com.google.sso.228293309116:/authCallback?login%3Dcode&sarp=1&delegated_client_id=228293309116-bs4u7ofpm4p6p6da7i1jkan3hfr6h38o.apps.googleusercontent.com&hl=ru-RU&device_name=iPhone&auth_extension=ADa53XKs5xbxIN7iKIwUHr0joOGUWDVJhmQ1eDCNchjRFAD4SDjEsGQnM270tD5gzdKeX4dcU2NFw0Q2SO9gZ-_hSaEwXp-0_zzh2-uO8heBgC7JAoxwCbk&system_version=11.2&app_version=1.0&kdlc=1&kdac=1&lib_ver=3.2&wv_mode=1")
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -72,31 +77,102 @@ class ViewController: UIViewController {
 	}
 
 	@IBAction func authenticate(_ sender: Any) {
-		// https://accounts.google.com/embedded/setup/v2/safarivc?as=6337ec33bd418784&scope=https://www.google.com/accounts/OAuthLogin+https://www.googleapis.com/auth/userinfo.email&client_id=936475272427.apps.googleusercontent.com&redirect_uri=com.google.sso.228293309116:/authCallback?login%3Dcode&sarp=1&delegated_client_id=228293309116.apps.googleusercontent.com&hl=ru&device_name=iPhone&auth_extension=ADa53XLlskm0lJdBfI2Eczzj7afLYzBB_iMjJ8MJGpvdDjio4zUdhfulKPWEacgpiLQPKp9sZXVlcKwevll3dSQ6pjk0ZxowTPytQW6JVFdWAMSMHQ5h51Q&lib_ver=1.0&wv_mode=1
-		
-		
-		let url2 = URL(string: "https://accounts.google.com/embedded/setup/v2/safarivc?as=6337ec33bd418784&scope=https://www.google.com/accounts/OAuthLogin+https://www.googleapis.com/auth/userinfo.email&client_id=936475272427.apps.googleusercontent.com&redirect_uri=com.google.sso.228293309116:/authCallback?login%3Dcode&sarp=1&delegated_client_id=228293309116.apps.googleusercontent.com&hl=ru&device_name=iPhone&auth_extension=ADa53XLlskm0lJdBfI2Eczzj7afLYzBB_iMjJ8MJGpvdDjio4zUdhfulKPWEacgpiLQPKp9sZXVlcKwevll3dSQ6pjk0ZxowTPytQW6JVFdWAMSMHQ5h51Q&lib_ver=1.0&wv_mode=1")!
-//		let url2 = URL(string: "https://accounts.google.com/embedded/setup/ios?scope=https://www.google.com/accounts/OAuthLogin+https://www.googleapis.com/auth/userinfo.email&client_id=936475272427.apps.googleusercontent.com&as=1f192cd4a1da333f&delegated_client_id=228293309116-bs4u7ofpm4p6p6da7i1jkan3hfr6h38o.apps.googleusercontent.com&hl=ru-RU&device_name=iPhone&auth_extension=ADa53XKobZj63dyWcBaPcrZq7JHa8SrOIVOd7h-dWaqKSmEz-VRIaUNjbK4mGQrntOXRzUDe_u1s9hpchAdkjxKYWtQPYQXWrbdSOb4MHzekEdgovjM3xLc&system_version=11.2.1&app_version=3.38.1007&kdlc=1&kdac=1")!
-		
-//		https://accounts.google.com/o/oauth2/v2/auth?response_type=code&code_challenge_method=S256&scope=openid%20profile&code_challenge=KfSdRCNa0pJTrEfhOAe8zKuLjt2y68b03hI4Khn4irI&redirect_uri=com.googleusercontent.apps.48866222172-2an2vqj4khhur1v9igjn3knd9et6lrkf:/oauth2redirect/google&client_id=48866222172-2an2vqj4khhur1v9igjn3knd9et6lrkf.apps.googleusercontent.com&state=vFe_XuJtIj5ofWIBcWUwgiXf34XhMKygbhTq_jzW1yI
-		
-		let working_Own_App = URL(string: "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&code_challenge_method=S256&scope=openid%20profile&code_challenge=KfSdRCNa0pJTrEfhOAe8zKuLjt2y68b03hI4Khn4irI&redirect_uri=com.googleusercontent.apps.48866222172-2an2vqj4khhur1v9igjn3knd9et6lrkf:/oauth2redirect/google&client_id=48866222172-2an2vqj4khhur1v9igjn3knd9et6lrkf.apps.googleusercontent.com&state=vFe_XuJtIj5ofWIBcWUwgiXf34XhMKygbhTq_jzW1yI")!
-		// com.googleusercontent.apps.48866222172-2an2vqj4khhur1v9igjn3knd9et6lrkf:/oauth2redirect/
-//		~!@#
-//		let url3 = URL(string: "https://accounts.google.com/embedded/setup/v2/safarivc?as=6337ec33bd418784&scope=https://www.google.com/accounts/OAuthLogin+https://www.googleapis.com/auth/userinfo.email&client_id=936475272427.apps.googleusercontent.com&redirect_uri=com.googleusercontent.apps.48866222172-2an2vqj4khhur1v9igjn3knd9et6lrkf:/oauth2redirect/&sarp=1&delegated_client_id=228293309116.apps.googleusercontent.com&hl=ru&device_name=iPhone&auth_extension=ADa53XLlskm0lJdBfI2Eczzj7afLYzBB_iMjJ8MJGpvdDjio4zUdhfulKPWEacgpiLQPKp9sZXVlcKwevll3dSQ6pjk0ZxowTPytQW6JVFdWAMSMHQ5h51Q&lib_ver=1.0&wv_mode=1")!
-		
-//		UIApplication.shared.open(url2!, options: [:], completionHandler: nil)
-//		print("open url: \(url)")
-//		let svc = SFSafariViewController(url: url3)
-//		svc.delegate = self
-//		present(svc, animated: true, completion: nil)
+		authAdvice { url in
+			print("advice url: \(url)")
+			DispatchQueue.main.async {
+				self.startAuthentiationSession(with: url)
+			}
+		}
+	}
+	
+	func startAuthentiationSession(with url: URL) {
 		
 		session = SFAuthenticationSession(url: url, callbackURLScheme: nil, completionHandler: { callbackUrl, error in
 			print("======== callbackurl: \(callbackUrl)")
 			print("======== error: \(error)")
 		})
-		
+
 		session.start()
+		
+//		let controller = SFSafariViewController(url: url)
+//		controller.delegate = self
+//		present(controller, animated: true, completion: nil)
+	}
+	
+//	func authAdvice2(callback: @escaping (URL) ->Void) {
+//		let body = "chrome_installed=false&client_id=228293309116.apps.googleusercontent.com&device_id=cba09321-d88f-4200-a3a7-c7d08d2a572c&device_name=iPhone&hl=ru&lib_ver=1.0&mediator_client_id=936475272427.apps.googleusercontent.com&package_name=com.google.PlayMusic&redirect_uri=com.google.sso.228293309116%3A%2FauthCallback"
+//
+//		var request = URLRequest(url: URL(string: "https://www.googleapis.com/oauth2/v3/authadvice")!)
+//		request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "content-type")
+//		request.addValue("GET", forHTTPHeaderField: "x-http-method-override")
+//		request.httpMethod = "POST"
+//		request.httpBody = body.data(using: .utf8)
+//
+//		URLSession.shared.dataTask(with: request) { data, response, error in
+//			print("advice response")
+//			if let data = data, let responseJson = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any] {
+//				if let url = responseJson["uri"] as? String {
+//					callback(URL(string: url)!)
+//				}
+//			}
+//
+//			if let error = error {
+//				print("error: \(error)")
+//			}
+//			}.resume()
+//	}
+	
+	func authAdvice(callback: @escaping (URL) ->Void) {
+		
+//		let json = 	"""
+//					{
+//					"report_user_id": "true",
+//					"system_version": "\(UIDevice.current.systemVersion)",
+//					"app_version": "1.0",
+//					"user_id": [],
+//					"request_trigger": "ADD_ACCOUNT",
+//					"lib_ver": "3.2",
+//					"package_name": "com.google.PlayMusic",
+//					"supported_service": ["uca"],
+//					"redirect_uri": "com.google.sso.myApp:/authCallback",
+//					"device_name": "iPhone",
+//					"fast_setup": "false",
+//					"mediator_client_id": "936475272427.apps.googleusercontent.com",
+//					"device_id": "\(UIDevice.current.identifierForVendor?.uuidString ?? UUID().uuidString)",
+//					"hl": "ru-RU",
+//					"client_id": "228293309116-bs4u7ofpm4p6p6da7i1jkan3hfr6h38o.apps.googleusercontent.com"
+//					}
+//					"""
+		let minimumJson =
+		"""
+		{
+		"redirect_uri": "com.google.sso.myApp:/authCallback",
+		"fast_setup": "false",
+		"mediator_client_id": "936475272427.apps.googleusercontent.com",
+		"device_id": "\(UIDevice.current.identifierForVendor?.uuidString ?? UUID().uuidString)",
+		"hl": "\(Locale.current.identifier)"
+		}
+		"""
+		
+//		print("request json: \(json)")
+		
+		var request = URLRequest(url: URL(string: "https://www.googleapis.com/oauth2/v3/authadvice")!)
+		request.addValue("application/json", forHTTPHeaderField: "content-type")
+		request.httpMethod = "POST"
+		request.httpBody = minimumJson.data(using: .utf8)
+		
+		URLSession.shared.dataTask(with: request) { data, response, error in
+			print("advice response")
+			if let data = data, let responseJson = (try? JSONSerialization.jsonObject(with: data, options: [])) as? [String: Any] {
+				if let url = responseJson["uri"] as? String {
+					callback(URL(string: url.replacingOccurrences(of: "&wv_mode=1", with: ""))!)
+				}
+			}
+			
+			if let error = error {
+				print("error: \(error)")
+			}
+		}.resume()
 	}
 }
 
