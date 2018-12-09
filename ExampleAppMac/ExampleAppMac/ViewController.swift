ef02b2d75c5b3ba2d21b6d1ede53a04c443052a9
Anton Efimenko <reloni@ya.ru>
2018-12-09 21:53:43 +0300

    Add empty mac project

---
+++ ExampleAppMac/ExampleAppMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExampleAppMac
+//
+//  Created by Anton Efimenko on 09/12/2018.
+//  Copyright © 2018 Anton Efimenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
