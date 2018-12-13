eaef5266281637f135d76e4b4c3cf5aade22054a
Anton Efimenko <reloni@ya.ru>
2018-12-13 10:18:58 +0300

    Remove old scripts

--- Scripts/DownloadCacheFromS3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-aws s3 sync s3://app-build-caches/RxGoogleMusic/Carthage/Build ./Carthage/Build/
-
-(cd Carthage/Build/iOS/ && for i in `find . | grep -iE "(.framework.tar.gz|.dsym.tar.gz)$"`; do tar -xzf "$i" ; done)
-(cd Carthage/Build/Mac/ && for i in `find . | grep -iE "(.framework.tar.gz|.dsym.tar.gz)$"`; do tar -xzf "$i" ; done)
