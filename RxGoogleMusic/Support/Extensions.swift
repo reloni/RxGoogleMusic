9e3c44f716b710fd6bc9bb8636d0637e47b2610e
Anton Efimenko <reloni@ya.ru>
2019-03-09 19:56:44 +0300

    Make extension on KeyedDecodingContainer internal

--- RxGoogleMusic/Support/Extensions.swift
+++ RxGoogleMusic/Support/Extensions.swift
@@ -57,11 +57,11 @@ extension Date {
 }
 
 extension KeyedDecodingContainer {
-	public func decode<T: Decodable>(_ key: Key, as type: T.Type = T.self) throws -> T {
+	func decode<T: Decodable>(_ key: Key, as type: T.Type = T.self) throws -> T {
 		return try self.decode(T.self, forKey: key)
 	}
 	
-	public func decodeIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) throws -> T? {
+	func decodeIfPresent<T: Decodable>(_ key: KeyedDecodingContainer.Key) throws -> T? {
 		return try decodeIfPresent(T.self, forKey: key)
 	}
 }
